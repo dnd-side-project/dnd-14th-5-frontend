1f351d34a4a7fccca44701d59b1f79e83f6c6193
baegyeong <beth0929@naver.com>
2026-01-25 16:52:28 +0900

    chore: husky로 git hook 지정
    - 커밋 전 prettier, eslint 실행
    - 커밋 메시지를 컨벤션에 맞게 적었는지 검사

---
+++ commitlint.config.mjs
@@ -0,0 +1,44 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'always'],
+
+    'subject-max-length': [2, 'always', 50],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [0],
+
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [0],
+
+    'footer-leading-blank': [0],
+    'footer-max-line-length': [0],
+
+    'body-starts-with-dash': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'body-starts-with-dash': (parsed) => {
+          const { body } = parsed;
+
+          if (!body) {
+            return [true];
+          }
+
+          const lines = body
+            .split('\n')
+            .filter((line) => line.trim().length > 0);
+          const allStartWithDash = lines.every((line) =>
+            line.trim().startsWith('-'),
+          );
+
+          if (!allStartWithDash) {
+            return [false, 'body의 각 줄은 "-"로 시작해야 합니다'];
+          }
+
+          return [true];
+        },
+      },
+    },
+  ],
+};
