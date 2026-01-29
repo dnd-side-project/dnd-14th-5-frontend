78dc1553e31ecfa86ce2989cc48cb6db4ab858f7
jeongyou <wjddbwjd1205@naver.com>
2026-01-30 02:38:16 +0900

    feat: 회고 작성 패이지에 헤더 추가

--- app/retrospective/page.tsx
+++ app/retrospective/page.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
+
+import PageHeader from '@/components/layout/PageHeader/PageHeader';
+
 const RetrospectivePage = () => {
   return (
-    <div className="bg-white min-h-screen">
-      <h1>Retrospective</h1>
+    <div className="bg-white min-h-screen px-6">
+      <PageHeader
+        title="회고 작성"
+        leftIcon={
+          <Image
+            src="/icons/chevron-left.svg"
+            alt="back"
+            width={24}
+            height={24}
+          />
+        }
+      />
     </div>
   );
 };
