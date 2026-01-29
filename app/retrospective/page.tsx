e2af12e23772a2e3be92405bfbeadf1943ee5ed4
jeongyou <wjddbwjd1205@naver.com>
2026-01-30 04:54:44 +0900

    feat: 회고 작성 페이지 구현

--- app/retrospective/page.tsx
+++ app/retrospective/page.tsx
@@ -1,21 +1,50 @@
 import Image from 'next/image';
 
+import TextArea from '@/components/features/retrospective/TextArea/TextArea';
+import BottomCTA from '@/components/layout/BottomCTA/BottomCTA';
 import PageHeader from '@/components/layout/PageHeader/PageHeader';
+import Button from '@/components/ui/Button/Button';
 
 const RetrospectivePage = () => {
   return (
-    <div className="bg-white min-h-screen px-6">
+    <div className="min-h-screen bg-[#292B37] text-white">
       <PageHeader
-        title="회고 작성"
+        title="기록하기"
         leftIcon={
           <Image
             src="/icons/chevron-left.svg"
             alt="back"
-            width={24}
-            height={24}
+            width={28}
+            height={28}
           />
         }
       />
+
+      <div className="mx-auto flex min-h-screen max-w-110 flex-col px-7.5 pt-10 pb-24">
+        <main className="flex flex-1 flex-col gap-6">
+          <section>
+            <div className="rounded-2xl  w-full bg-[#555557] p-4">
+              <p className="text-[17px] font-semibold text-white">
+                오늘 하루 중 가장 재미있었던 순간은 언제였나요?
+              </p>
+              <p className="text-[13px] font-normal text-[#ECECEC] opacity-50">
+                지금 떠오르는 감정이나 생각을 부담없이 작성해보세요!
+              </p>
+            </div>
+          </section>
+
+          <section className="flex flex-col gap-3">
+            <p className="text-[17px] font-bold text-white px-2">
+              오늘 나의 생각은?
+            </p>
+            <TextArea />
+          </section>
+        </main>
+      </div>
+
+      <BottomCTA>
+        <Button label="기록 완료" />
+      </BottomCTA>
     </div>
   );
 };
