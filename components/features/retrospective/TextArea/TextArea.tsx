b303ccb3a22a7aec03913c5d6bc56edb5aba561a
jeongyou <wjddbwjd1205@naver.com>
2026-01-30 04:53:20 +0900

    feat: TextArea 컴포넌트 구현

---
+++ components/features/retrospective/TextArea/TextArea.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { type ComponentPropsWithoutRef, useState } from 'react';
+
+type TextAreaProps = Omit<
+  ComponentPropsWithoutRef<'textarea'>,
+  'value' | 'defaultValue'
+> & {
+  value?: string;
+  defaultValue?: string;
+  variant?: 'default';
+  maxLength?: number;
+};
+
+const TextArea = ({
+  value,
+  defaultValue,
+  onChange,
+  variant = 'default',
+  maxLength = 3000,
+  ...props
+}: TextAreaProps) => {
+  const [innerValue, setInnerValue] = useState(defaultValue ?? '');
+
+  const isControlled = value !== undefined;
+  const displayValue = isControlled ? value : innerValue;
+
+  const handleChange: ComponentPropsWithoutRef<'textarea'>['onChange'] = (
+    e,
+  ) => {
+    if (!isControlled) setInnerValue(e.currentTarget.value);
+    onChange?.(e);
+  };
+
+  const styles = {
+    default: {
+      wrapper: 'rounded-[24px] border border-white/60 bg-[#2B2F3D]',
+      textarea:
+        'min-h-[360px] w-full resize-none bg-transparent px-5  my-4 text-[14px] text-white placeholder:text-white/60 focus:outline-none disabled:opacity-50',
+      counter: 'flex justify-end px-5 pb-4 text-[14px] text-white',
+    },
+  }[variant];
+
+  return (
+    <div className={`${styles.wrapper}`}>
+      <textarea
+        className={`${styles.textarea} scrollbar-transparent-track`}
+        value={displayValue}
+        onChange={handleChange}
+        maxLength={maxLength}
+        placeholder="어떤 일이 있었나요?"
+        {...props}
+      />
+
+      <div className={styles.counter}>
+        {displayValue.length}/{maxLength}자
+      </div>
+    </div>
+  );
+};
+
+export default TextArea;
