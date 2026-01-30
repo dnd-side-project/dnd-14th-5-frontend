f4d48991fe71abd50669949df556b2b059e24eee
jeongyou <wjddbwjd1205@naver.com>
2026-01-30 21:49:10 +0900

    chore: 스토리북 설정 업데이트

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,7 @@ const config: StorybookConfig = {
   stories: [
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/**/*.stories.@(ts|tsx)',
   ],
   addons: [
     '@chromatic-com/storybook',
