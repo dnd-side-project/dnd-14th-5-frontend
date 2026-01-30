284c87e7179fd42fa8e33e50df6762a942061b00
baegyeong <beth0929@naver.com>
2026-01-31 01:36:26 +0900

    feat: ProgressBar 컴포넌트 구현
    - 진행도를 계산하는 유틸함수 분리

---
+++ src/components/ui/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,19 @@
+import { calculatePercentage } from '@/lib/helpers/calculateProgress';
+
+interface ProgressBarProps {
+  current: number;
+  max: number;
+}
+
+function ProgressBar({ current, max }: ProgressBarProps) {
+  return (
+    <div className="w-full bg-g-30 rounded-full h-3">
+      <div
+        className="bg-g-300 h-3 rounded-full text-xs flex items-center justify-center text-white"
+        style={{ width: `${calculatePercentage({ current, max })}%` }}
+      />
+    </div>
+  );
+}
+
+export default ProgressBar;
