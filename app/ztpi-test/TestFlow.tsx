8313975f23134aee2a4c92f67333040ece1a6a6d
baegyeong <beth0929@naver.com>
2026-02-04 00:16:00 +0900

    feat: TestFlow 컴포넌트에서 step 타입 정의 추가

--- app/ztpi-test/TestFlow.tsx
+++ app/ztpi-test/TestFlow.tsx
@@ -7,8 +7,10 @@ import InProgress from '@/src/components/features/test/InProgress/InProgress';
 import Intro from '@/src/components/features/test/Intro/Intro';
 import Result from '@/src/components/features/test/Result/Result';
 
+type Step = 'intro' | 'progress' | 'completed' | 'result';
+
 const TestFlow = () => {
-  const [step, setStep] = useState('progress');
+  const [step, setStep] = useState<Step>('progress'); // TODO: 현재 진행 중인 개발 페이지가 progress라서 임시로 초기화
 
   switch (step) {
     case 'intro':
