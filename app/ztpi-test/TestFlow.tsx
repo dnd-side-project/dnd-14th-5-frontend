7f0374cc86b25452b6d501e2ad7e27865432e39f
baegyeong <beth0929@naver.com>
2026-02-07 22:45:48 +0900

    chore: ZTPI 테스트 시작 페이지 삭제
    - 온보딩 후 바로 테스트가 시작되기 때문에 첫 시작 페이지 삭제

--- app/ztpi-test/TestFlow.tsx
+++ app/ztpi-test/TestFlow.tsx
@@ -4,17 +4,14 @@ import { useState } from 'react';
 
 import Completed from '@/src/components/features/test/Completed/Completed';
 import InProgress from '@/src/components/features/test/InProgress/InProgress';
-import Intro from '@/src/components/features/test/Intro/Intro';
 import Result from '@/src/components/features/test/Result/Result';
 
-type Step = 'intro' | 'progress' | 'completed' | 'result';
+type Step = 'progress' | 'completed' | 'result';
 
 const TestFlow = () => {
   const [step, setStep] = useState<Step>('progress'); // TODO: 현재 진행 중인 개발 페이지가 progress라서 임시로 초기화
 
   switch (step) {
-    case 'intro':
-      return <Intro />;
     case 'progress':
       return <InProgress />;
     case 'completed':
