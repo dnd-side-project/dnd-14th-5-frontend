437ed273c4bb52deba7e78aeb70ea846efa59d9d
jeongyou <wjddbwjd1205@naver.com>
2026-02-13 22:27:43 +0900

    refactor: 피드백 카드, 완료 버튼 UI 분리

--- app/reflection/[id]/feedback/page.tsx
+++ app/reflection/[id]/feedback/page.tsx
@@ -1,42 +1,21 @@
-import Image from 'next/image';
-
+import CompleteButton from '@/src/components/features/reflectionFeedback/CompleteButton/CompleteButton';
+import ResultCard from '@/src/components/features/reflectionFeedback/ResultCard/ResultCard';
 import BottomCTA from '@/src/components/layout/BottomCTA/BottomCTA';
 import PageHeader from '@/src/components/layout/PageHeader/PageHeader';
-import Button from '@/src/components/ui/Button/Button';
-import Card from '@/src/components/ui/Card/Card';
-import Icon from '@/src/components/ui/Icon/Icon';
 
-const feedbackPage = () => {
+const FeedbackPage = () => {
   return (
     <div className="flex h-dvh flex-col">
       <PageHeader title="피드백" />
       <div className="px-7.5">
-        {/* TODO: 카드 컴포넌트로 교체 */}
-        <Card className="h-117.5 flex flex-col px-8 gap-6 items-center justify-center mt-10 bg-y-50 text-g-900">
-          <Image
-            src="/character/past-negative-shadow.svg"
-            alt="여기 각 캐릭터 이름으로 대체"
-            width={150}
-            height={150}
-          />
-          {/* TODO: 배지 컴포넌트로 교체 */}
-          <div className="flex items-center gap-2 w-34 h-6 px-6 text-caption-n bg-g-0 rounded-lg">
-            <Icon name="check" size={16} />
-            <p>첫 회고 달성!</p>
-          </div>
-          <p className="text-heading-h3">오늘은 과거를 많이 떠올리셨어요</p>
-          <p className="text-body-m text-g-200 text-justify wrap-break-word">
-            이 기억이 지금의 나에게 어떤 의미를 주고 있는지도 한번 생각해 볼 수
-            있어요. 내일은 순간을 더 즐겨보는 건 어떨까요?
-          </p>
-        </Card>
+        <ResultCard />
       </div>
 
       <BottomCTA>
-        <Button label="완료" />
+        <CompleteButton />
       </BottomCTA>
     </div>
   );
 };
 
-export default feedbackPage;
+export default FeedbackPage;
