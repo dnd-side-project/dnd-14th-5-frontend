f0d646e291b12da1c5751b6a3c52c896398cbf36
jeongyou <wjddbwjd1205@naver.com>
2026-02-14 05:34:57 +0900

    feat: 버튼에 페이지 이동 기능 추가

--- src/components/features/home/HomeActionsSection/HomeActionsSection.tsx
+++ src/components/features/home/HomeActionsSection/HomeActionsSection.tsx
@@ -1,13 +1,27 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import Button from '@/src/components/ui/Button/Button';
 
 const HomeActionsSection = () => {
+  const router = useRouter();
+
+  const handleSkip = () => {
+    // Todo: 건너뛰기 기능 구현 - 어떻게 할지 논의 필요
+  };
+
+  const handleAnswer = () => {
+    router.push('/reflection');
+  };
+
   return (
     <div className="flex items-center gap-3">
       <div className="flex-1">
-        <Button label="건너뛰기" variant="secondary" />
+        <Button label="건너뛰기" variant="secondary" onClick={handleSkip} />
       </div>
       <div className="flex-2">
-        <Button label="답변하기" />
+        <Button label="답변하기" onClick={handleAnswer} />
       </div>
     </div>
   );
