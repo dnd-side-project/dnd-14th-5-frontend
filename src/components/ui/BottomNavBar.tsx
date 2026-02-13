b36a517ea4b15d22b0b9382fc49203e6d7c951ed
jeongyou <wjddbwjd1205@naver.com>
2026-02-14 04:49:04 +0900

    feat: 하단 네비 버튼 아이콘 active 전환 적용

--- src/components/ui/BottomNavBar.tsx
+++ src/components/ui/BottomNavBar.tsx
@@ -3,18 +3,34 @@
 import { useState } from 'react';
 
 import BottomNavButton, { type TabKey } from './BottomNavButton';
+import type { IconNameType } from './Icon/Icon.types';
+
+interface BottomTabItem {
+  key: TabKey;
+  activeIconName: IconNameType;
+  inactiveIconName: IconNameType;
+}
+
+const bottomTabs: BottomTabItem[] = [
+  {
+    key: 'calendar',
+    activeIconName: 'calendarActive',
+    inactiveIconName: 'calendarInactive',
+  },
+  {
+    key: 'home',
+    activeIconName: 'homeActive',
+    inactiveIconName: 'homeInactive',
+  },
+  {
+    key: 'profile',
+    activeIconName: 'userActive',
+    inactiveIconName: 'userInactive',
+  },
+];
 
 const BottomNavBar = () => {
   const [activeMenu, setActiveMenu] = useState<TabKey>('home');
-  const tabs: TabKey[] = ['calendar', 'home', 'profile'];
-
-  const getIconClassName = (key: TabKey) => {
-    if (key === activeMenu) {
-      return 'bg-primary';
-    }
-
-    return 'bg-g-0/80';
-  };
 
   const handleTabClick = (tabKey: TabKey) => {
     setActiveMenu(tabKey);
@@ -24,12 +40,13 @@ const BottomNavBar = () => {
     <div className="fixed bottom-6 left-1/2 z-10 w-full h-16 max-w-105 -translate-x-1/2 px-5">
       <div className="flex items-center justify-center">
         <div className="flex w-full items-center justify-between rounded-full bg-g-500 px-6 py-3">
-          {tabs.map((tabKey) => (
+          {bottomTabs.map((tab) => (
             <BottomNavButton
-              key={tabKey}
-              tabKey={tabKey}
-              isActive={activeMenu === tabKey}
-              iconClassName={getIconClassName(tabKey)}
+              key={tab.key}
+              tabKey={tab.key}
+              isActive={activeMenu === tab.key}
+              activeIconName={tab.activeIconName}
+              inactiveIconName={tab.inactiveIconName}
               onClick={handleTabClick}
             />
           ))}
