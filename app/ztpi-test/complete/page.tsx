453de62b7247f9b08e95da2d414e0122a49c64c4
baegyeong <beth0929@naver.com>
2026-02-18 00:31:21 +0900

    feat: ZTPI 테스트 완료 UI 구현

---
+++ app/ztpi-test/complete/page.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+import Button from '@/src/components/ui/Button/Button';
+
+const TestCompletePage = () => {
+  return (
+    <div className="flex flex-col items-center justify-between h-full">
+      <div className="flex flex-1 flex-col justify-center items-center space-y-15 mb-50">
+        <div className="text-center space-y-2">
+          <h2 className="text-heading-h2 text-g-0">테스트 완료!</h2>
+          <p className="text-body-m text-g-60">나의 시간관은 무엇일까요?</p>
+        </div>
+        <Image
+          src="/character/success-test.gif"
+          alt="character gif"
+          width={200}
+          height={200}
+        />
+      </div>
+
+      <Button label="결과 확인하기" className="text-g-900 h-10" />
+    </div>
+  );
+};
+
+export default TestCompletePage;
