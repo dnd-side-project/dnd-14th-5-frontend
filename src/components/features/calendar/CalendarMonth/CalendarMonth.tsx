b6884245a39a3b8172c97ed489999eb54fca7a90
jeongyou <wjddbwjd1205@naver.com>
2026-02-19 07:09:40 +0900

    feat: 월간 캘린더 구현

---
+++ src/components/features/calendar/CalendarMonth/CalendarMonth.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { format } from 'date-fns';
+import { useMemo } from 'react';
+
+import { useMonthReflection } from '../../reflection/queries/useMonthReflection';
+import { useCalendarState } from '../hooks/useCalendarState';
+import { getCategoryTypeByDate } from '../utils/getCategoryTypeByDate';
+import { mapReflectionItems } from '../utils/mapReflectionItems';
+import CalendarMonthGrid from './CalendarMonthGrid/CalendarMonthGrid';
+import CalendarMonthHeader from './CalendarMonthHeader/CalendarMonthHeader';
+import CalendarMonthWeekdays from './CalendarMonthWeekdays/CalendarMonthWeekdays';
+
+const CalendarMonth = () => {
+  const {
+    today,
+    currentMonth,
+    currentMonthLabel,
+    selectedDate,
+    days,
+    goPrevMonth,
+    goNextMonth,
+    selectDate,
+  } = useCalendarState();
+  const formattedMonth = format(currentMonth, 'yyyy-MM');
+  const { data } = useMonthReflection({ month: formattedMonth });
+
+  const categoryTypeByDate = useMemo(
+    () => getCategoryTypeByDate(mapReflectionItems(data ?? [])),
+    [data],
+  );
+
+  return (
+    <div className="space-y-3">
+      <CalendarMonthHeader
+        currentMonthLabel={currentMonthLabel}
+        goPrevMonth={goPrevMonth}
+        goNextMonth={goNextMonth}
+      />
+
+      <CalendarMonthWeekdays />
+
+      {/* TODO: 아이콘 바뀐걸로 변경해야 함. */}
+      <CalendarMonthGrid
+        days={days}
+        currentMonth={currentMonth}
+        selectedDate={selectedDate}
+        today={today}
+        categoryTypeByDate={categoryTypeByDate}
+        selectDate={selectDate}
+      />
+    </div>
+  );
+};
+
+export default CalendarMonth;
