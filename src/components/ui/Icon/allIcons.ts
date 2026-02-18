5c520a01129ae6f0c80ce34e3f230ccf3e8653f1
jeongyou <wjddbwjd1205@naver.com>
2026-02-19 04:09:04 +0900

    chore: 캘린더 관련 아이콘 추가

--- src/components/ui/Icon/allIcons.ts
+++ src/components/ui/Icon/allIcons.ts
@@ -1,6 +1,13 @@
 const allIcons = {
   chevronLeft: '/icons/chevron-left.svg',
   fire: '/icons/fire.svg',
+  calendarDayPastN: '/icons/calendar-day-past-n.svg',
+  calendarDayPastP: '/icons/calendar-day-past-p.svg',
+  calendarDayPresentH: '/icons/calendar-day-present-h.svg',
+  calendarDayPresentF: '/icons/calendar-day-present-f.svg',
+  calendarDayFuture: '/icons/calendar-day-future.svg',
+  calendarDaySlate: '/icons/calendar-day-slate.svg',
+  calendarDayOutline: '/icons/calendar-day-outline.svg',
 } as const;
 
 export default allIcons;
