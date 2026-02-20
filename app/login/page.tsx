bc7642ab1c5523920de6ad02e2508d06289f8813
baegyeong <beth0929@naver.com>
2026-02-21 02:09:12 +0900

    feat: 로그인 페이지에 소셜 로그인 버튼 추가 및 관련 SVG 파일 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,20 @@
+import LoginButton from '@/src/components/features/onboarding/LoginButton/LoginButton';
+
 const LoginPage = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col justify-between h-full">
+      <section className="flex flex-col items-center justify-center py-60">
+        <>로고</>
+        <p className="text-body-m text-g-0">시간 성향에 맞춘 나만의 회고</p>
+      </section>
+
+      <section className="space-y-4">
+        <LoginButton provider="kakao" />
+        <LoginButton provider="naver" />
+        <LoginButton provider="google" />
+      </section>
+    </div>
+  );
 };
 
 export default LoginPage;
---
+++ src/components/features/onboarding/LoginButton/LoginButton.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { type ComponentProps } from 'react';
+
+import Button from '@/src/components/ui/Button/Button';
+import { cn } from '@/src/lib/helpers/cn';
+
+interface LoginButtonProps extends Omit<
+  ComponentProps<typeof Button>,
+  'label' | 'variant'
+> {
+  provider: 'kakao' | 'naver' | 'google';
+}
+
+const PROVIDER_CONFIG = {
+  kakao: {
+    label: '카카오로 시작하기',
+    className:
+      "bg-[#FEE500] text-g-900 before:bg-[url('/onboarding/kakao-logo.svg')]",
+  },
+  naver: {
+    label: '네이버로 시작하기',
+    className:
+      "bg-[#02C75A] text-g-0 before:bg-[url('/onboarding/naver-logo.svg')]",
+  },
+  google: {
+    label: 'Google로 시작하기',
+    className:
+      "bg-g-0 text-g-800 before:bg-[url('/onboarding/google-logo.svg')]",
+  },
+} as const;
+
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const LoginButton = ({ provider, ...props }: LoginButtonProps) => {
+  const config = PROVIDER_CONFIG[provider];
+
+  const handleLogin = () => {
+    window.location.href = `${baseURL}auth/login/${provider}`;
+  };
+
+  return (
+    <Button
+      label={config.label}
+      className={cn(
+        'flex items-center justify-center gap-2 before:content-[""] before:block before:w-4 before:h-4 before:bg-contain before:bg-no-repeat h-10',
+        config.className,
+      )}
+      onClick={handleLogin}
+      {...props}
+    />
+  );
+};
+
+export default LoginButton;
