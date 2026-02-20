3a544c84c539b64777a602ed151bc8cc036730ad
jeongyou <wjddbwjd1205@naver.com>
2026-02-21 05:20:13 +0900

    feat: reflectionId 값 없을 때 홈으로 리다이렉트 처리 기능 추가

--- app/reflection/[id]/feedback/page.tsx
+++ app/reflection/[id]/feedback/page.tsx
@@ -8,7 +8,7 @@ interface FeedbackPageProps {
 const FeedbackPage = async ({ params }: FeedbackPageProps) => {
   const { id } = await params;
   const reflectionId = Number(id);
-  
+
   return (
     <div className="flex h-dvh flex-col">
       <PageHeader title="피드백" />
