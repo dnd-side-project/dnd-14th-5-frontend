5a8166b5ed7783110bcfda676dddf702d2724fff
jeongyou <wjddbwjd1205@naver.com>
2026-02-22 05:35:50 +0900

    refactor: 변경된 디자인 반영

--- src/components/features/reflectionFeedback/ResultCard/ResultCard.tsx
+++ src/components/features/reflectionFeedback/ResultCard/ResultCard.tsx
@@ -19,7 +19,14 @@ const ResultCard = ({ feedback, category }: ResultCardProps) => {
   const categoryMessage = getCategoryMessage(category);
 
   return (
-    <Card className="flex h-117.5 flex-col items-center justify-center gap-6 bg-y-50 text-g-900">
+    <Card className="flex max-h-117.5 flex-col items-center justify-center gap-6 py-10 px-8 bg-g-500">
+      <Badge variant="neutral">
+        <div className="flex items-center gap-1">
+          <Icon name="check" size={16} />
+          <p>{streakDays}일 연속 회고 중!</p>
+        </div>
+      </Badge>
+
       <Image
         src="/character/past-negative-shadow.svg"
         alt="여기 각 캐릭터 이름으로 대체"
@@ -27,18 +34,13 @@ const ResultCard = ({ feedback, category }: ResultCardProps) => {
         height={150}
       />
 
-      <Badge variant="secondary">
-        <div className="flex items-center gap-1">
-          <Icon name="check" size={16} />
-          <p>{streakDays}일 연속 회고 중!</p>
-        </div>
-      </Badge>
-
-      <p className="text-heading-h3">{categoryMessage}</p>
+      <p className="text-heading-h3 text-g-0">{categoryMessage}</p>
       {/* TODO: 바뀐 텍스트 토큰으로 업데이트 필요 */}
-      <p className="wrap-break-word text-justify text-body-m text-g-200">
-        {feedback}
-      </p>
+      <div className="max-h-65 overflow-y-auto pr-2">
+        <p className="wrap-break-word text-justify text-body-s text-g-80">
+          {feedback}
+        </p>
+      </div>
     </Card>
   );
 };
