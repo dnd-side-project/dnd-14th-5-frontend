1bf29b6f5c920edbc0ee457872c13a3ac6ab1b22
baegyeong <beth0929@naver.com>
2026-02-23 23:48:49 +0900

    feat: BottomNavBar 컴포넌트 추가

--- app/reflection/[reflectionId]/page.tsx
+++ app/reflection/[reflectionId]/page.tsx
@@ -1,5 +1,6 @@
 import Detail from '@/src/components/features/reflectionDetail/Detail/Detail';
 import Header from '@/src/components/features/reflectionDetail/Header/Header';
+import BottomNavBar from '@/src/components/layout/BottomNavBar/BottomNavBar';
 
 interface PageProps {
   params: Promise<{
@@ -11,9 +12,10 @@ const Page = async ({ params }: PageProps) => {
   const { reflectionId } = await params;
 
   return (
-    <div className="space-y-10">
+    <div className="space-y-10 pb-20">
       <Header />
       <Detail reflectionId={+reflectionId} />
+      <BottomNavBar />
     </div>
   );
 };
