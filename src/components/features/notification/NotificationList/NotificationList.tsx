1761bacb67d8d4e67c236b34cd85e54d5d2df9ac
jeongyou <wjddbwjd1205@naver.com>
2026-02-23 22:54:10 +0900

    feat: 알림 읽음 처리 로직 NotificationList에 연결

--- src/components/features/notification/NotificationList/NotificationList.tsx
+++ src/components/features/notification/NotificationList/NotificationList.tsx
@@ -1,20 +1,41 @@
 'use client';
 
+import { useNotificationReadActions } from '../hooks/useNotificationReadActions';
 import NotificationEmptyState from '../NotificationEmptyState/NotificationEmptyState';
-import { useNotificationHistoriesQuery } from '../queries/useNotificationsHistoryQuery';
+import NotificationItem from '../NotificationItem/NotificationItem';
+import NotificationReadAllButton from '../NotificationReadAllButton/NotificationReadAllButton';
 
 const NotificationList = () => {
-  const { data } = useNotificationHistoriesQuery();
+  const { notifications, hasNotifications, readAll, readOne } =
+    useNotificationReadActions();
 
-  if (data?.length === 0) {
+  if (!hasNotifications) {
     return (
-      <div className="flex flex-col h-full items-center justify-center pb-30">
+      <div className="flex h-full flex-col items-center justify-center pb-30">
         <NotificationEmptyState />
       </div>
     );
   }
 
-  return <>{data}</>;
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="flex justify-end pb-2">
+        <NotificationReadAllButton
+          disabled={!hasNotifications}
+          onClick={readAll}
+        />
+      </div>
+      {notifications.map((notification) => (
+        <NotificationItem
+          key={notification.id}
+          id={notification.id}
+          notifiedAt={notification.notifiedAt}
+          title={notification.title}
+          onDelete={readOne}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default NotificationList;
