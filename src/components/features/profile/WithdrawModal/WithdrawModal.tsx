412c5320b25704107223781ce3627eaddd444735
jeongyou <wjddbwjd1205@naver.com>
2026-02-24 06:39:28 +0900

    feat: 탈퇴하기 모달 구현

---
+++ src/components/features/profile/WithdrawModal/WithdrawModal.tsx
@@ -0,0 +1,53 @@
+import Button from '@/src/components/ui/Button/Button';
+import Modal from '@/src/components/ui/Modal/Modal';
+
+interface WithdrawModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  isCloseDisabled?: boolean;
+  isConfirmDisabled?: boolean;
+}
+
+const WithdrawModal = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  isCloseDisabled = false,
+  isConfirmDisabled = false,
+}: WithdrawModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-3 text-center">
+          <h3 className="text-heading-h3 text-g-0">정말 떠나시나요?</h3>
+          <div className="text-body-s text-g-80">
+            <p>지금까지 남긴 회고와 기록이 모두 삭제돼요.</p>
+            <p>삭제된 기록은 다시 복구할 수 없어요.</p>
+            <p>그래도 탈퇴하시겠어요?</p>
+          </div>
+        </div>
+
+        <div className="flex gap-3">
+          <Button
+            label="계속 기록하기"
+            variant="secondary"
+            onClick={onClose}
+            disabled={isCloseDisabled}
+            className="bg-g-400 h-11"
+          />
+
+          <Button
+            label="탈퇴하기"
+            variant="primary"
+            onClick={onConfirm}
+            disabled={isConfirmDisabled}
+            className="h-11"
+          />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default WithdrawModal;
