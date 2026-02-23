95bb9cae697b1df2272598cdaeafbce53bcf2548
jeongyou <wjddbwjd1205@naver.com>
2026-02-24 05:52:52 +0900

    refactor: 라벨 이름 수정

--- src/components/features/profile/AccountSection/AccountSection.tsx
+++ src/components/features/profile/AccountSection/AccountSection.tsx
@@ -7,9 +7,8 @@ import { MenuRow } from '../MenuRow/MenuRow';
 const AccountSection = () => {
   return (
     <Card className="flex flex-col gap-4 bg-g-400 ">
-      <p className="text-body-s font-bold">시간관 캐릭터 테스트</p>
-      <MenuRow label="테스트 다시하기" onClick={() => {}} />
-      <MenuRow label="지난 테스트결과 보기" onClick={() => {}} />
+      <MenuRow label="로그아웃" onClick={() => {}} />
+      <MenuRow label="탈퇴하기" onClick={() => {}} />
     </Card>
   );
 };
