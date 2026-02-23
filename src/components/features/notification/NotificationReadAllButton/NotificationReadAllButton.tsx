35a906bfb7c90784236c7f812189ae035c21230f
jeongyou <wjddbwjd1205@naver.com>
2026-02-23 22:17:06 +0900

    feat: 모두 읽음 버튼 컴포넌트 추가

---
+++ src/components/features/notification/NotificationReadAllButton/NotificationReadAllButton.tsx
@@ -0,0 +1,22 @@
+interface NotificationReadAllButtonProps {
+  disabled: boolean;
+  onClick: () => void;
+}
+
+const NotificationReadAllButton = ({
+  disabled,
+  onClick,
+}: NotificationReadAllButtonProps) => {
+  return (
+    <button
+      type="button"
+      className="text-caption-n text-g-100 disabled:cursor-not-allowed disabled:text-g-300"
+      disabled={disabled}
+      onClick={onClick}
+    >
+      모두 읽기
+    </button>
+  );
+};
+
+export default NotificationReadAllButton;
