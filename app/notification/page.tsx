68dc9ba70267b14d8c4ff167464474a11374661f
jeongyou <wjddbwjd1205@naver.com>
2026-02-23 18:56:03 +0900

    chore: 페이지 이름 변경

---
+++ app/notification/page.tsx
@@ -0,0 +1,5 @@
+const NotificationPage = () => {
+  return <div>푸시알림 확인 페이지</div>;
+};
+
+export default NotificationPage;
--- app/notifications/page.tsx
+++
@@ -1,5 +0,0 @@
-const NotificationsPage = () => {
-  return <div>푸시알림 확인 페이지</div>;
-};
-
-export default NotificationsPage;
