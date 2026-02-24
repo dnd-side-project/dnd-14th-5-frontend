6459b4e7794ec32c63c866806be0b9321a7f35c4
baegyeong <beth0929@naver.com>
2026-02-24 11:22:55 +0900

    feat: 로그인 페이지에 로고 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image';
+
 import LoginButton from '@/src/components/features/onboarding/LoginButton/LoginButton';
 
 const LoginPage = () => {
   return (
     <div className="flex flex-col justify-between h-full">
-      <section className="flex flex-col items-center justify-center py-60">
-        <>로고</>
+      <section className="flex flex-col items-center justify-center py-55 gap-2">
+        <Image
+          src="/icons/timo-title.svg"
+          alt="timo"
+          width={150}
+          height={150}
+        />
         <p className="text-body-m text-g-0">시간 성향에 맞춘 나만의 회고</p>
       </section>
 
