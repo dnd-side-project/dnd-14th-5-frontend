70b1f54b2044a9dbfa76d2b24988deab28d93b90
jeongyou <wjddbwjd1205@naver.com>
2026-02-25 11:53:56 +0900

    refactor: 불필요한 config 제거

--- src/components/features/reflection/queries/useTodayQuestionQuery.ts
+++ src/components/features/reflection/queries/useTodayQuestionQuery.ts
@@ -2,7 +2,6 @@ import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
 import { z } from 'zod';
 
 import { get } from '@/src/lib/api';
-import type { ApiRequestConfig } from '@/src/lib/api/schema';
 
 import { reflectionKeys } from '../constants/queryKeys';
 import { REFLECTION_ENDPOINTS } from '../constants/url';
@@ -18,14 +17,10 @@ export const todayQuestionSchema = z.object({
 
 export type TodayQuestionResponse = z.infer<typeof todayQuestionSchema>;
 
-export const getTodayQuestion = async (
-  config?: ApiRequestConfig<never, never, TodayQuestionResponse>,
-) => {
-  return get<TodayQuestionResponse>(REFLECTION_ENDPOINTS.todayQuestion, {
-    ...config,
+export const getTodayQuestion = async () =>
+  get<TodayQuestionResponse>(REFLECTION_ENDPOINTS.todayQuestion, {
     responseSchema: todayQuestionSchema,
   });
-};
 
 export const useTodayQuestionQuery = () =>
   useQuery({
