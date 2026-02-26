0bab54c958e721d79c0adfa3dab91fcd83d5c300
jeongyou <wjddbwjd1205@naver.com>
2026-02-26 22:38:26 +0900

    feat: 테스트 메뉴 이동 로직 연결

--- src/components/features/profile/TestSection/TestSection.tsx
+++ src/components/features/profile/TestSection/TestSection.tsx
@@ -2,14 +2,17 @@
 
 import Card from '@/src/components/ui/Card/Card';
 
+import { useTestSectionActions } from '../hooks/useTestSectionActions';
 import { MenuRow } from '../MenuRow/MenuRow';
 
 const TestSection = () => {
+  const { handleRetakeTest, handleViewLatestResult } = useTestSectionActions();
+
   return (
     <Card className="flex flex-col gap-4 bg-g-400 ">
       <p className="text-body-s font-bold">시간관 캐릭터 테스트</p>
-      <MenuRow label="테스트 다시하기" onClick={() => {}} />
-      <MenuRow label="지난 테스트결과 보기" onClick={() => {}} />
+      <MenuRow label="테스트 다시하기" onClick={handleRetakeTest} />
+      <MenuRow label="지난 테스트결과 보기" onClick={handleViewLatestResult} />
     </Card>
   );
 };
