99abbfc89aa1074ab8096b21c620927db0db4297
jeongyou <wjddbwjd1205@naver.com>
2026-03-26 18:20:05 +0900

    fix: 하단 CTA 여백 조정

--- src/components/layout/BottomCTA/BottomCTA.tsx
+++ src/components/layout/BottomCTA/BottomCTA.tsx
@@ -6,7 +6,7 @@ interface BottomCTAProps {
 
 const BottomCTA = ({ children }: BottomCTAProps) => {
   return (
-    <div className="fixed bottom-0 left-1/2 z-10 w-full max-w-110 -translate-x-1/2 px-7.5 py-4">
+    <div className="fixed bottom-0 left-1/2 z-10 w-full max-w-110 -translate-x-1/2 px-7.5 pt-4 pb-8">
       <div className="flex items-center justify-center">{children}</div>
     </div>
   );
