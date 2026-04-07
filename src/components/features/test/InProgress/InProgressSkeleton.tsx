241e4ed568aa392af3f13e918d94cfd733303419
baegyeong <beth0929@naver.com>
2026-04-08 00:37:36 +0900

    fix: ztpi 테스트 스켈레톤 레이아웃이 붙어보이는 현상 수정

--- src/components/features/test/InProgress/InProgressSkeleton.tsx
+++ src/components/features/test/InProgress/InProgressSkeleton.tsx
@@ -2,9 +2,11 @@ import Skeleton from '@/src/components/ui/Skeleton/Skeleton';
 
 const InProgressSkeleton = () => {
   return (
-    <div className="flex flex-col justify-between h-full pt-2">
+    <div className="flex flex-col min-h-full pt-2">
       <Skeleton className="h-2" />
-      <Skeleton className="h-140 py-20" />
+      <div className="flex flex-1 items-center justify-center py-6">
+        <Skeleton className="h-140 w-full" />
+      </div>
       <div className="flex gap-3">
         <Skeleton className="h-10 w-full" />
         <Skeleton className="h-10 w-full" />
