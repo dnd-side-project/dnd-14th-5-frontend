3bb3ed4b732527339556b855b7f1870967ebc09c
jeongyou <wjddbwjd1205@naver.com>
2026-05-15 16:33:59 +0900

    feat: 그룹 리스트 인터페이스 정의

---
+++ src/components/features/groups/GroupList/GroupList.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+interface GroupItem {
+  id: string;
+  name: string;
+  type: 'friend' | 'character'; //TODO: 임시 타입 정의, 백엔드 정의에 맞춰 수정해야 함
+  image: string;
+}
+
+interface GroupListProps {
+  groups: GroupItem[];
+}
+
+const GroupList = ({ groups }: GroupListProps) => {
+  return (
+    <div>
+      {groups.map((item) => (
+        <div key={item.id}>
+          <Image src={item.image} alt={item.name} width={100} height={100} />
+          <p>{item.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GroupList;
